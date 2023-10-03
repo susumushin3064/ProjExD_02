d5bfaa5de8e8edaab21e1c7ff8d56b83c7256d2e
susumushin <c0a22099b5@edu.teu.ac.jp>
2023-10-03 15:43:44 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,18 +6,40 @@ import pygame as pg
 WIDTH, HEIGHT = 1600, 900
 
 
+delta = {
+    pg.K_UP: (0, 5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
+
+def check_bound(obj_rct: pg.Rect):
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)
+
     bd_img = pg.Surface((20, 20)) #爆弾サーフェイスを設定する
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)
+    vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
@@ -26,12 +48,34 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        if kk_rct.colliderect(bd_rct):
+            print("ゲームオーバー")
+            return
+        
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+
+        bd_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bd_img, bd_rct)
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
